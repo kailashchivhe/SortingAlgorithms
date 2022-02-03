a63110138177f66029817f6276814b8511ea303a
Kailash Chivhe <kchivhe@gmail.com>
2022-02-03 17:27:26 -0500

    Printed 3 results to output files.

--- src/com/kailash/Main.java
+++ src/com/kailash/Main.java
@@ -2,9 +2,19 @@ package com.kailash;
 
 import com.kailash.helper.SortHelper;
 
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.PrintStream;
+
 public class Main {
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws FileNotFoundException {
+        /*
+        * To print output to file uncomment the below code.
+        * */
+//        PrintStream o = new PrintStream(new File("output3.txt"));
+//        PrintStream console = System.out;
+//        System.setOut(o);
 
         System.out.println("*******************************************************************************");
         System.out.println("***>><<<*>>> Time Complexities for below input sizes: <<<*>>><<***");
@@ -12,5 +22,7 @@ public class Main {
         System.out.println("*******************************************************************************");
 
         SortHelper.initiateSorting();
+
+//        System.setOut(console);
     }
 }
