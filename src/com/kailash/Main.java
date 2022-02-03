91ae2a7fba365cff7a49dc48f939878889591772
Kailash Chivhe <kchivhe@gmail.com>
2022-02-03 16:36:29 -0500

    Uncommented initiation

--- src/com/kailash/Main.java
+++ src/com/kailash/Main.java
@@ -11,6 +11,6 @@ public class Main {
         System.out.println("*>><<<*>>>   1K 2K 4K 5K 10K 20K 40K 50K 60K 80K 100K <<<*>>><<*");
         System.out.println("*******************************************************************************");
 
-//        SortHelper.initiateSorting();
+        SortHelper.initiateSorting();
     }
 }
